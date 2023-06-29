830a7abcde4f306cc15e7a64b10712e15d33a9c2
Guilherme-Joviniano <00drpixelss@gmail.com>
2023-06-29 09:05:33 -0300

    feat: ensure that AccountMongoRepository returns a account when success

--- src/v1/infra/database/mongodb/account-repository/account.ts
+++ src/v1/infra/database/mongodb/account-repository/account.ts
@@ -1,7 +1,13 @@
+import { MongoHelper } from '../helpers/mongo.helper'
 import { type Account, type AddAccountRepository, type AddAccountSchema } from './account.protocol'
 
 export class AccountRepository implements AddAccountRepository {
-  async execute (account: AddAccountSchema): Promise<Account> {
+  async add (accountData: AddAccountSchema): Promise<Account> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const { insertedId } = await accountCollection.insertOne(accountData)
+    const { _id, ...accountWithoutId } = await accountCollection.findOne({ _id: insertedId })
+    const account = Object.assign({}, accountWithoutId, { id: _id.toHexString() }) as Account
 
+    return account
   }
 }
