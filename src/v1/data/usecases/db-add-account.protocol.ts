1b7365805e2749bc4874b686cc11d4eb20d40a24
Guilherme-Joviniano <00drpixelss@gmail.com>
2023-06-29 07:40:17 -0300

    refactor: changing files

--- src/v1/data/usecases/db-add-account.protocol.ts
+++
@@ -1,4 +0,0 @@
-export * from '../protocols/encrypter.protocol'
-export * from '../protocols/db-add-usecase.protocol'
-export * from '../../domain/schemas/add-account.schema'
-export * from '../../domain/entities/account.entity'
