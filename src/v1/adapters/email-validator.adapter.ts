b7caaeab9dea79a71c300f4439950b973ab80f9e
Guilherme-Joviniano <00drpixelss@gmail.com>
2023-06-29 08:02:30 -0300

    refactor: deleting the files of old path

--- src/v1/adapters/email-validator.adapter.ts
+++
@@ -1,8 +0,0 @@
-import { type EmailValidator } from '../presentations/protocols/email-validator.protocol'
-import validator from 'validator'
-
-export class EmailValidatorAdapter implements EmailValidator {
-  isValid (email: string): boolean {
-    return validator.isEmail(email)
-  }
-}
